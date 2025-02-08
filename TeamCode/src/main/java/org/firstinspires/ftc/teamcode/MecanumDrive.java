736cb95acf6163a1864335e3ed495186019aad1d
JasperW7 <jasperwang14@gmail.com>
2025-02-08 10:44:32 -0500

    rr values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -84,7 +84,7 @@ public final class MecanumDrive {
         // path controller gains
         public double axialGain = 0;
         public double lateralGain = 0;
-        public double headingGain = 9; // shared with turn
+        public double headingGain = 0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
